a93509b7b3b4826fc490018f2a11f104e2213682
Ruben <rubenca2312@gmail.com>
2023-05-25 09:36:33 +0200

    First commit

---
+++ src/app/prueba1/prueba1.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-prueba1',
+  templateUrl: './prueba1.component.html',
+  styleUrls: ['./prueba1.component.css']
+})
+export class Prueba1Component {
+  msg: String;
+  constructor() {
+    this.msg = "HELLO WORLD!";
+  }
+}
+
